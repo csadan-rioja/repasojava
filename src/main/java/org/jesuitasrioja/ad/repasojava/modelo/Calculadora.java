bf237daf98fcdafb5d0e83af324409d8d437e52f
Carlos <csaenzadan@jesuitasformacion.com>
2021-05-12 10:12:13 +0200

    primer commit

---
+++ src/main/java/org/jesuitasrioja/ad/repasojava/modelo/Calculadora.java
@@ -0,0 +1,17 @@
+package org.jesuitasrioja.ad.repasojava.modelo;
+
+public class Calculadora {
+	
+	public int suma(int a, int b) {
+		return 0;
+	}
+	
+	public int resta(int a, int b) {
+		return 0;
+	}
+	
+	public double division(double a, double b) {
+		return 0;
+	}
+
+}
---
+++ src/test/java/org/jesuitasrioja/ad/repasojava/modelo/CalculadoraTest.java
@@ -0,0 +1,26 @@
+package org.jesuitasrioja.ad.repasojava.modelo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+	
+	Calculadora c = new Calculadora();
+
+	@Test
+	public void testSuma() {
+		assertEquals(2, c.suma(1, 1));
+	}
+
+	@Test
+	public void testResta() {
+		assertEquals(2, c.resta(4,2));
+	}
+
+	@Test
+	public void testDivision() {
+		assertEquals(Double.valueOf(2.0), Double.valueOf(c.division(4.0, 2.0)));
+	}
+
+}
