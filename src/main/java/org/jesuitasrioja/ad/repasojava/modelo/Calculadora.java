d842b00d3deba718f6b05e4b2227979dae2d4c2c
Carlos <csaenzadan@jesuitasformacion.com>
2021-05-12 13:02:38 +0200

    resuelto

--- src/main/java/org/jesuitasrioja/ad/repasojava/modelo/Calculadora.java
+++ src/main/java/org/jesuitasrioja/ad/repasojava/modelo/Calculadora.java
@@ -3,15 +3,15 @@ package org.jesuitasrioja.ad.repasojava.modelo;
 public class Calculadora {
 	
 	public int suma(int a, int b) {
-		return 0;
+		return a+b;
 	}
 	
 	public int resta(int a, int b) {
-		return 0;
+		return a-b;
 	}
 	
 	public double division(double a, double b) {
-		return 0;
+		return a/b;
 	}
 
 }
