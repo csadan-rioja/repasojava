ec70c3ff6410dd8c5cec6040e81086de67180ce8
Carlos Sáenz-Adán <csaenzadan@jesuitasformacion.com>
2021-05-13 10:52:30 +0200

    desarreglado

--- src/main/java/org/jesuitasrioja/ad/repasojava/modelo/Calculadora.java
+++ src/main/java/org/jesuitasrioja/ad/repasojava/modelo/Calculadora.java
@@ -3,15 +3,12 @@ package org.jesuitasrioja.ad.repasojava.modelo;
 public class Calculadora {
 	
 	public int suma(int a, int b) {
-		return a+b;
 	}
 	
 	public int resta(int a, int b) {
-		return a-b;
 	}
 	
 	public double division(double a, double b) {
-		return a/b;
 	}
 
 }
