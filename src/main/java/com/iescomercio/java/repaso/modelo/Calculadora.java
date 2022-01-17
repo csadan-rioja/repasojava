857c6f61ee3cc035960885628b0932eeb44c746d
Carlos <csaenz@iescomercio.com>
2022-01-17 11:51:33 +0100

    sin errores

--- src/main/java/com/iescomercio/java/repaso/modelo/Calculadora.java
+++ src/main/java/com/iescomercio/java/repaso/modelo/Calculadora.java
@@ -2,13 +2,19 @@ package com.iescomercio.java.repaso.modelo;
 
 public class Calculadora {
 	
+	//implemetacion de la suma de dos numeros
 	public int suma(int a, int b) {
+		return 0;
 	}
 	
+	//implementacion de la resta de dos numeros
 	public int resta(int a, int b) {
+		return 0;
 	}
 	
+	//implementacion de la division de dos numeros
 	public double division(double a, double b) {
+		return 0;
 	}
 
 }
